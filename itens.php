e9319a1230618e041f4d0327702918e2b22499e2
PremierSoft <matheus.angelicio00@gmail.com>
2021-11-24 12:40:55 -0300

    Listar itens

--- itens.php
+++ itens.php
@@ -10,6 +10,11 @@
         header('Location: login.php');
     }
 
+    // Query para pegar os dados
+    $sql = "SELECT * FROM itens ORDER BY id DESC";
+
+    $result = $conexao->query($sql);
+
 ?>
 
 <!DOCTYPE html>
@@ -45,6 +50,52 @@
             <a href="sair.php" class="btn btn-danger me-5">Sair</a>
         </div>
     </nav>
+
+    <div class="m-5">
+    <table class="table text-white table-bg">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Produto</th>
+                    <th scope="col">Descrição</th>
+                    <th scope="col">Categoria</th>
+                    <th scope="col">Qtd Estoque</th>
+                    <th scope="col">...</th>
+                </tr>
+            </thead>
+        <tbody>
+        <?php
+                while($user_data = mysqli_fetch_assoc($result)){
+                    // Montando a tabela
+                    echo "<tr>";
+                    echo "<td>".$user_data['id']."</td>";
+                    echo "<td>".$user_data['produto']."</td>";
+                    echo "<td>".$user_data['descricao']."</td>";
+                    echo "<td>".$user_data['categoria']."</td>";
+                    echo "<td>".$user_data['estoque']."</td>";
+                    echo "<td>
+                        <a class='btn btn-sm btn-primary' href=''>
+                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
+                            <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z'/>
+                        </svg>
+                        </a>
+                        <a class='btn btn-sm btn-danger' href=''>
+                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash-fill' viewBox='0 0 16 16'>
+                                 <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z'/>
+                            </svg>
+                        </a>
+                        <a class='btn btn-sm btn-secondary' href=''>
+                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-arrow-down-square' viewBox='0 0 16 16'>
+                                <path fill-rule='evenodd' d='M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z'/>
+                            </svg>
+                        </a>
+                    </td>";
+                    echo "</tr>";
+                }
+            ?>
+        </tbody>
+        </table>
+    </div>
 
 
     
