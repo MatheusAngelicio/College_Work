2a6f7525d6d3922c949fb4062d8a2975c89f0695
PremierSoft <matheus.angelicio00@gmail.com>
2021-11-24 16:34:26 -0300

    Adicionar opcao de deletar item

---
+++ deleteItens.php
@@ -0,0 +1,23 @@
+<?php
+    if(!empty($_GET['id'])){
+        // Incluindo conexao com banco de dados
+        include_once('config.php');
+
+        // Atribuindo a variavel id o id que vem da URL
+        $id = $_GET['id'];
+    
+        // Verifica se esse id existe no meu banco de dados
+        $sqlSelect = "SELECT * FROM itens WHERE id = $id";
+    
+        $result = $conexao->query($sqlSelect);
+    
+        // Se result foir maior que 0, existe o registro e eles esta apto para ser deletado
+        if($result->num_rows > 0){
+            // Se achar o id na consulta
+            $sqlDelete = "DELETE FROM itens WHERE id=$id";
+            $resultDelete = $conexao->query($sqlDelete);
+        }
+    }
+
+    header('Location: itens.php');
+?>
