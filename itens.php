5f94e1e3db7ff064e439f7b77533a372345d2561
PremierSoft <matheus.angelicio00@gmail.com>
2021-11-24 13:55:44 -0300

    Emprestar itens

--- itens.php
+++ itens.php
@@ -34,6 +34,23 @@
         .table-bg{
             background: rgba(0, 0, 0, 0.3);
             border-radius: 15px 15px 0 0;
+            margin-top: 100px;
+        }
+        .box{
+            position: absolute;
+            top: 16.5%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            background-color: rgba(0, 0, 0, 0.6);
+            padding: 10px;
+            border-radius: 10px;
+        }
+        a{
+            text-decoration: none;
+            color: white;
+            border: 3px ;
+            border-radius: 50px;
+            padding: 10px;
         }
         </style>
 </head>
@@ -84,7 +101,7 @@
                                  <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z'/>
                             </svg>
                         </a>
-                        <a class='btn btn-sm btn-secondary' href=''>
+                        <a class='btn btn-sm btn-secondary' href='emprestar.php?id=$user_data[id]'>
                             <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-arrow-down-square' viewBox='0 0 16 16'>
                                 <path fill-rule='evenodd' d='M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z'/>
                             </svg>
@@ -97,6 +114,9 @@
         </table>
     </div>
 
+    <div class="box">
+        <a href=" ">Adicionar Item</a>
+    </div>
 
     
 </body>
